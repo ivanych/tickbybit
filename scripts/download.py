3ae714dc93a800ca177904de29b4a529ced8baa1
Mikhail Ivanov <m.ivanych@gmail.com>
2024-04-23 00:20:05 +0300

    Скрипт tickers переименован в download

--- scripts/download.py
+++ scripts/download.py
@@ -9,6 +9,7 @@ async def main():
     new_tickers = await tickers()
     await save(new_tickers, dirpath='.tickers')
 
-    pruned = prune(dirpath='.tickers', period=5*60*1000)
+    pruned = prune(period=5 * 60 * 1000, interval=60 * 1000, dirpath='.tickers')
+
 
 asyncio.run(main())
