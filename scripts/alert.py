74d4b92610b8b15b74a18acee9ab48f311052bac
Mikhail Ivanov <m.ivanych@gmail.com>
2024-04-24 00:37:24 +0300

    Скрипты отладки переделаны на сериализацию pickle

--- scripts/alert.py
+++ scripts/alert.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python
 
+import asyncio
+
 from tickbybit import settings
 from tickbybit.files import pair
 
-# Настройки
-settings = settings(dirpath='.settings')
 
-# Пара сравниваемых прайсов
-tickers_pair = pair(settings, dirpath='.tickers')
+async def main():
+    # Настройки
+    settings_dict = settings(dirpath='.settings')
 
-# Изменения в отслеживаемых тикерах
-tickers_diff = tickers_pair.diff(settings)
+    # Пара сравниваемых прайсов
+    tickers_pair = await pair(settings_dict, dirpath='.tickers')
 
-# Вывод изменений
-diffs = tickers_diff.alert()
+    # Изменения в отслеживаемых тикерах
+    tickers_diff = tickers_pair.diff(settings_dict)
 
-if diffs:
-    for ticker_diff in diffs:
-        print(ticker_diff.to_json())
-else:
-    print("Уведомлений нет.")
+    # Вывод изменений
+    diffs = tickers_diff.alert()
+
+    if diffs:
+        for ticker_diff in diffs:
+            print(ticker_diff.to_json())
+    else:
+        print("Уведомлений нет.")
+
+
+asyncio.run(main())
