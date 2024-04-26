af3f02998f9bef978df793d369bdabcc548a719c
Mikhail Ivanov <m.ivanych@gmail.com>
2024-04-27 02:21:21 +0300

    Добавлены форматы tpl1p и tpl2p

--- src/tickbybit/settings.py
+++ src/tickbybit/settings.py
@@ -62,7 +62,7 @@ def set_key(dirpath: str, path: str, value: Any = None) -> dict:
         jsonvalue = True if value == 'true' else False
 
     elif re.match('format$', path):
-        vals = ['json', 'yaml', 'str1', 'str2', 'str3', 'str4']
+        vals = ['json', 'yaml', 'str1', 'str2', 'str3', 'str4', 'tpl1p', 'tpl2p']
         val = '|'.join(vals)
         assert re.match(f'({val})$', value), f'Допустимые значения: {vals}'
 
