58ba60aff89d07269958f19693fb875eeb73127b
Mikhail Ivanov <m.ivanych@gmail.com>
2024-05-04 01:48:43 +0300

    Модуль schedule переименован в tickers

--- src/tickbybit/schedule/tickers.py
+++ src/tickbybit/schedule/tickers.py
@@ -11,5 +11,5 @@ async def download_new_tickers(dirpath: str) -> None:
     await save(new_tickers['tickers'], time=new_tickers['time'], dirpath=dirpath)
 
 
-async def prune_old_tickers(period: int, interval: int, dirpath: str) -> None:
-    prune(period=period, interval=interval, dirpath=dirpath)
+async def prune_old_tickers(dirpath: str, ttl: int) -> None:
+    prune(dirpath=dirpath, ttl=ttl)
