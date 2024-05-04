1678e69db9e631e75d5236c11dadcd9390b69ebc
Mikhail Ivanov <m.ivanych@gmail.com>
2024-05-05 00:00:29 +0300

    Мелкие правки, форматирование

--- src/tickbybit/schedule/tickers.py
+++ src/tickbybit/schedule/tickers.py
@@ -10,6 +10,10 @@ async def download_new_tickers(dirpath: str) -> None:
     new_tickers = await tickers()
     await save(new_tickers['tickers'], time=new_tickers['time'], dirpath=dirpath)
 
+    logger.info("Загружен новый прайс")
+
 
 async def prune_old_tickers(dirpath: str, ttl: int) -> None:
     prune(dirpath=dirpath, ttl=ttl)
+
+    logger.info("Удалены старые старые прайсы")
